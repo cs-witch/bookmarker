4d80ea7b8ee799872d9fc8b1fe2e22b57e260f74
Andisa Jones <andisa.andisa@gmail.com>
2018-10-12 01:53:55 -0400

    html, css , js added

---
+++ script.js
@@ -0,0 +1,11 @@
+/* create an alert for each submission.
+alert for Site Name and URL each time it's entered. 
+go to udemy and re-watch tut.
+
+function nameSite() { ...
+
+ 	//is there a way to double-check if an entry is correct? how do we do this?
+}
+
+*/
+
